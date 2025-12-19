d03ccd2d4039e75cf58a5787a994ca2b4b25fd7a
Saurav Gami <saurav.g@acidintegrations.com>
2025-12-19 22:45:22 +0545

    update data handling after api update in profile api

--- src/store/features/auth/authSlice.ts
+++ src/store/features/auth/authSlice.ts
@@ -1,31 +1,39 @@
-'use client'
+"use client";
 
-
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface AuthState {
-    isAuthenticated: boolean;
-    user: null | { id: string; name: string };
+  isAuthenticated: boolean;
+  user: null | { id: string; name: string };
+  guide?: any;
 }
 
 export const initialState: AuthState = {
-    isAuthenticated: false,
-    user: null,
+  isAuthenticated: false,
+  user: null,
+  guide: null,
 };
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers: {
-        login(state, action: PayloadAction<{ id: string; name: string }>) {
-            state.isAuthenticated = true;
-            state.user = action.payload;
-        },
-        logout(state) {
-            state.isAuthenticated = false;
-            state.user = null;
-        },
+  name: "auth",
+  initialState,
+  reducers: {
+    login(
+      state,
+      action: PayloadAction<{
+        user: { id: string; name: string };
+        guideProfile?: any;
+      }>
+    ) {
+      state.isAuthenticated = true;
+      state.user = action.payload?.user;
+      state.guide = action.payload?.guideProfile;
     },
+    logout(state) {
+      state.isAuthenticated = false;
+      state.user = null;
+    },
+  },
 });
 
 export const { login, logout } = authSlice.actions;
